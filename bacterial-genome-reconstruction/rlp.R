fdbbb4155f57490254cc5bf76844d053ac1a1fa5
Rom Grk <romgrk.cc@gmail.com>
2020-09-11 13:48:10 -0400

    rlp: lint

--- bacterial-genome-reconstruction/rlp.R
+++ bacterial-genome-reconstruction/rlp.R
@@ -3,7 +3,7 @@
 #options(repos="https://cran.rstudio.com",verbose = F )
 list.of.packages <-
     c("magrittr", "dplyr", "data.table")
-new.packages <- 
+new.packages <-
     list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)>0) install.packages(new.packages)
 suppressMessages(require(magrittr))
@@ -15,15 +15,15 @@ args <- commandArgs(trailingOnly=TRUE)
 cutoffs <- as.character(args[1])
 cutoff_path <- as.character(args[2])
 
-rl_cutoffs <- 
+rl_cutoffs <-
     as.numeric(unlist(strsplit(cutoffs, split = ",")))
 
 
-perfect_alignments <- 
+perfect_alignments <-
     lapply(
-        rl_cutoffs, 
+        rl_cutoffs,
         function(cutoff_number){
-            blast_table_m3 <- 
+            blast_table_m3 <-
                 fread(
                     sprintf(
                         "%s/cutoff_%i.csv",
@@ -32,8 +32,8 @@ perfect_alignments <-
                     )
                 ) %>%
                 `colnames<-`(c(
-                    "qseqid", "sseqid", "length", 
-                    "pident", "mismatch", 
+                    "qseqid", "sseqid", "length",
+                    "pident", "mismatch",
                     "gapopen", "slen", "qlen", "bitscore")) %>%
                 mutate(npid = round((length*0.01*pident*100)/slen)) %>%
                 filter(npid == 100) %>%
@@ -42,16 +42,16 @@ perfect_alignments <-
     ) %>% unlist
 
 
-m3_summary_table <- 
+m3_summary_table <-
     tibble(
-        Cutoff = 
+        Cutoff =
             rl_cutoffs,
         Perfect_hits = perfect_alignments
     ) %>%
     mutate(product = Perfect_hits * Cutoff)
 
 write.csv(
-    m3_summary_table, 
+    m3_summary_table,
     sprintf(
         "output/module_3/summary_table.csv"
         )
