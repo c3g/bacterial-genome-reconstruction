cee4dd81393b3de2c01fa43daf6abc3573168376
Rom Grk <romgrk.cc@gmail.com>
2020-09-18 15:19:17 -0400

    client: fix fastqInput reducer

--- client/src/reducers/fastqInput.js
+++ client/src/reducers/fastqInput.js
@@ -11,25 +11,60 @@ const initialState = {
   },
 }
 
+window.files = new Map()
+
 export const fastqInput = createSlice({
   name: 'fastqInput',
   initialState: initialState,
   reducers: {
     setFiles: (state, action) => {
-      state.r1.file = action.payload.r1 === null ? state.r1.file : action.payload.r1
-      state.r2.file = action.payload.r2 === null ? state.r2.file : action.payload.r2
+      if (action.payload.r1 !== undefined) {
+        if (state.r1.file)
+          window.files.delete(state.r1.file)
+        state.r1.file = action.payload.r1
+      }
+      if (action.payload.r2 !== undefined) {
+        if (state.r2.file)
+          window.files.delete(state.r2.file)
+        state.r2.file = action.payload.r2
+      }
     },
     setMessages: (state, action) => {
       state.r1.message = action.payload.r1 === null ? state.r1.message : action.payload.r1
       state.r2.message = action.payload.r2 === null ? state.r2.message : action.payload.r2
     },
     clear: (state) => {
+      if (state.r1.file)
+        window.files.delete(state.r1.file)
+      if (state.r2.file)
+        window.files.delete(state.r2.file)
       state.r1 = initialState.r1
       state.r2 = initialState.r2
     }
   },
 });
 
-export const { setFiles, setMessages, clear } = fastqInput.actions;
+export const { setMessages, clear } = fastqInput.actions;
+
+export const setFiles = (payload) => {
+  let r1, r2
+  if (payload.r1 !== undefined) {
+    if (payload.r1 === null) {
+      r1 = null
+    } else {
+      r1 = payload.r1.name
+      window.files.set(payload.r1.name, payload.r1)
+    }
+  }
+  if (payload.r2 !== undefined) {
+    if (payload.r2 === null) {
+      r2 = null
+    } else {
+      r2 = payload.r2.name
+      window.files.set(payload.r2.name, payload.r2)
+    }
+  }
+  return fastqInput.actions.setFiles({ r1, r2 })
+}
 
 export default fastqInput.reducer;
