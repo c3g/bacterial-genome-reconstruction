9b6b8a31121989a43e57989b61aa75f8bac6e045
Rom Grk <romgrk.cc@gmail.com>
2020-09-25 17:48:53 -0400

    scripts: update setup/index.sh

--- bacterial-genome-reconstruction/scripts/setup/index.sh
+++ bacterial-genome-reconstruction/scripts/setup/index.sh
@@ -3,5 +3,5 @@ set -euf
 set -o pipefail
 
 ./scripts/setup/create_directories.sh
+./scripts/setup/download_data.sh
 ./scripts/setup/create_blast_databases.sh
-./scripts/setup/create_blast_databases_by_genus.sh
