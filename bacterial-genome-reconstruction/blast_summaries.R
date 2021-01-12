5e1ead4bcf4d3c996398b29ecc78fc0f2ad62228
Rom Grk <romgrk.cc@gmail.com>
2021-01-12 14:45:37 -0500

    fix: bug when no results in blast_summaries.R

--- bacterial-genome-reconstruction/blast_summaries.R
+++ bacterial-genome-reconstruction/blast_summaries.R
@@ -10,7 +10,7 @@ suppressMessages(require(magrittr))
 suppressMessages(require(dplyr))
 suppressMessages(require(data.table))
 
-#arguments
+# Arguments
 args <- commandArgs(trailingOnly=TRUE)
 stats_path <- as.character(args[1])
 blast_path <- as.character(args[2])
@@ -25,69 +25,70 @@ size <- file.size(blast_path)
 if (size == 0) {
     write.csv(list(), summary_path)
     write.csv(list(), read_length_path)
-} else {
-    ## add a column for true percent identity, and filter
-    blast_table <-
-        fread(
-            blast_path
-        ) %>%
-        `colnames<-`(c(
-            "qseqid", "sseqid", "length",
-            "pident", "mismatch",
-            "gapopen", "slen", "qlen", "bitscore", "stitle")) %>%
-        mutate(npid = round((length*0.01*pident*100)/qlen)) %>%
-        filter(npid >= 95)
-
-
-    ## Report a summary of each database hit
-    out_table_hits <-
-        blast_table %>%
-        mutate(
-            sseqid =
-                unlist(
-                    lapply(
-                        sseqid,
-                        function(x){ strsplit(x, "\\|")[[1]][2]}
-                    )
-                )
-        ) %>%
-        group_by(sseqid) %>%
-        summarise(
-            total_count=n(),
-            total_bitscore = sum(bitscore),
-            slen = unique(slen),
-            reads = 2*stats_table$num_seqs,
-            exp_coverage =
-                round(
-                    ((total_count * stats_table$avg_len)*(reads/1000))/
-                        unique(slen), 2
-                ),
-            name = first(stitle)
-        ) %>%
-        arrange(desc(total_count)) %>%
-        `colnames<-`(
-            c(
-                "accession", "total_count",
-                "total_bitscore", "genome_length", "total_reads",
-                "expected_coverage", "name"
-
-            )
-        ) %>%
-        as.data.frame()
-
-    write.csv(
-        out_table_hits,
-        summary_path
-    )
-
-    ## then get the count of each read length
-    out_table_read_length <-
-        blast_table %>%
-        dplyr::count(qlen) %>%
-        as.data.frame()
-
-    write.csv(
-        out_table_read_length,
-        read_length_path
-    )
+    quit("no", 0)
 }
+
+# add a column for true percent identity, and filter
+blast_table <-
+    fread(
+        blast_path
+    ) %>%
+    `colnames<-`(c(
+        "qseqid", "sseqid", "length",
+        "pident", "mismatch",
+        "gapopen", "slen", "qlen", "bitscore", "stitle")) %>%
+    mutate(npid = round((length*0.01*pident*100)/qlen)) %>%
+    filter(npid >= 95)
+
+
+if (nrow(blast_table) == 0) {
+    write.csv(list(), summary_path)
+    write.csv(list(), read_length_path)
+    quit("no", 0)
+}
+
+# Report a summary of each database hit
+out_table_hits <-
+    blast_table %>%
+    mutate(
+        sseqid =
+            unlist(
+                lapply(
+                    sseqid,
+                    function(x){ strsplit(x, "\\|")[[1]][2]}
+                )
+            )
+    ) %>%
+    group_by(sseqid) %>%
+    summarise(
+        total_count=n(),
+        total_bitscore = sum(bitscore),
+        slen = unique(slen),
+        reads = 2*stats_table$num_seqs,
+        exp_coverage =
+            round(
+                ((total_count * stats_table$avg_len)*(reads/1000))/
+                    unique(slen), 2
+            ),
+        name = first(stitle)
+    ) %>%
+    arrange(desc(total_count)) %>%
+    `colnames<-`(
+        c(
+            "accession", "total_count",
+            "total_bitscore", "genome_length", "total_reads",
+            "expected_coverage", "name"
+
+        )
+    ) %>%
+    as.data.frame()
+
+# then get the count of each read length
+out_table_read_length <-
+    blast_table %>%
+    dplyr::count(qlen) %>%
+    as.data.frame()
+
+# Write results
+write.csv(out_table_hits, summary_path)
+write.csv(out_table_read_length, read_length_path)
